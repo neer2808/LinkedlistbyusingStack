93cf34816fa5a8dae8e8c5935713942cfa3f31a4
neer2808 <uic.neerajkhanna@gmail.com>
2019-03-10 15:02:43 +0530

    create method to check linked list is empty or not

--- src/nodepackage/Linkedlistwithstack.java
+++ src/nodepackage/Linkedlistwithstack.java
@@ -5,11 +5,17 @@ public class Linkedlistwithstack {
 
   public void push(int data)
   {
+  Node n = new Node();
+  n.setData(data);
 
   }
 
+  // Method to check link list is empty or not
+
+
   public void pop()
   {
+
 
   }
 }
